8ec5414190e6d2515e32295e14a1b95029cc88a8
Naoki Imai <naokinetin0110@gmail.com>
2021-02-14 14:46:09 +0900

    rm migration

--- db/migrate/20210211074830_drp.rb
+++
@@ -1,5 +0,0 @@
-class Drp < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :typings
-  end
-end
--- db/migrate/20210211075704_drop.rb
+++
@@ -1,6 +0,0 @@
-class Drop < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :gits
-    drop_table :rails
-  end
-end
